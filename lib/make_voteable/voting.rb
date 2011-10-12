53e6a4b8362e5a5f8b43387fdd9372c1a874a986
Johan Buts <johan.buts@trustedarea.com>
2011-10-12 09:12:07 +0200

    Added some helper scopes for the voting model (credits thumbs_up)

--- lib/make_voteable/voting.rb
+++ lib/make_voteable/voting.rb
@@ -4,5 +4,10 @@ module MakeVoteable
 
     belongs_to :voteable, :polymorphic => true
     belongs_to :voter, :polymorphic => true
+
+    scope :for_voter, lambda { |*args| where(["voter_id = ? AND voter_type = ?", args.first.id, args.first.class.name]) }
+    scope :for_voteable, lambda { |*args| where(["voteable_id = ? AND voteable_type = ?", args.first.id, args.first.class.name]) }
+    scope :recent, lambda { |*args| where(["created_at > ?", (args.first || 2.weeks.ago)]) }
+    scope :descending, order("created_at DESC")
   end
 end
