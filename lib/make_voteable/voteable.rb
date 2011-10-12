05ce90fc5631ef61e9a589121cc612989792e9b2
Johan Buts <johan.buts@trustedarea.com>
2011-10-12 09:44:23 +0200

    Added some more lookup methods to the voteable model (credits thumbs_up)

--- lib/make_voteable/voteable.rb
+++ lib/make_voteable/voteable.rb
@@ -17,5 +17,35 @@ module MakeVoteable
     def votes
       self.up_votes - self.down_votes
     end
+
+    def votes_up
+      Voting.where(:voteable_id => id, :voteable_type => self.class.name, :up_vote => true).count
+    end
+
+    def votes_down
+      Voting.where(:voteable_id => id, :voteable_type => self.class.name, :up_vote => false).count
+    end
+
+    def percent_up
+      (votes_up.to_f * 100 / (self.votings.size + 0.0001)).round
+    end
+
+    def percent_down
+      (votes_down.to_f * 100 / (self.votings.size + 0.0001)).round
+    end
+
+    def voters_who_voted
+      self.votings.map(&:voter).uniq
+    end
+
+    def voted_by?(voter)
+      Voting.where(
+        :voteable_id => self.id,
+        :voteable_type => self.class.name,
+        :voter_type => voter.class.name,
+        :voter_id => voter.id
+      ).exists?
+    end
+
   end
 end
